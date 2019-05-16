85fc16369960f94e8f481ea2d87f2e66eafabf6b
Tatiane Takeuti <tatiane.takeuti@hotmail.com>
2019-05-16 08:58:11 -0300

    Using File() and Scanner() java class

---
+++ src/application/ReadingFile.java
@@ -0,0 +1,31 @@
+package application;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class ReadingFile {
+
+	public static void main(String[] args) {
+
+		File file = new File("c:\\temp\\in.txt");
+		Scanner sc = null;
+		try {
+			sc = new Scanner(file);
+			while (sc.hasNextLine()) {
+				System.out.println(sc.nextLine());
+			}
+
+		} catch (IOException e) {
+			System.out.println("Error: " + e);
+			System.out.println();
+			System.out.println("Error: " + e.getMessage());
+		} finally {
+			if (sc != null) {
+				sc.close();
+			}
+		}
+
+	}//main(-)
+
+}//class(-)
